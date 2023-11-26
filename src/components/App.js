147130e931ae5e0770b5db025174fc2ff3eb9724
sourabh1428 <sppathak1428@gmail.com>
2023-11-26 17:23:04 +0530

    asd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[inp,setInp]=useState('');
+
+  function handleChange(event){
+   if(event.target.value.length<14){
+
+  if(event.target.value.length==1)setInp("("+event.target.value);
+    else if(event.target.value.length==4){
+      setInp(event.target.value+")");
+    }
+    else if(event.target.value.length==8){
+      setInp(event.target.value+"-");
+    }
+    else{
+      setInp(event.target.value);
+    }
+  }
+  }
+  function handleClick(){
+
+    setInp('');
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={handleChange} value={inp}/>
+        <button onClick={handleClick}>Submit</button>
     </div>
   )
 }

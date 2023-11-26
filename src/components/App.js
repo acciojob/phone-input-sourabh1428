4824c1c5b32a36dafbb9af9b369ed4ef285dd887
sourabh1428 <sppathak1428@gmail.com>
2023-11-26 17:28:43 +0530

    Ad

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,13 @@ const App = () => {
   const[inp,setInp]=useState('');
 
   function handleChange(event){
-   if(event.target.value.length<14){
+   if(event.target.value.length<15){
 
   if(event.target.value.length==1)setInp("("+event.target.value);
     else if(event.target.value.length==4){
-      setInp(event.target.value+")");
+      setInp(event.target.value+") ");
     }
-    else if(event.target.value.length==8){
+    else if(event.target.value.length==9){
       setInp(event.target.value+"-");
     }
     else{
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <input type="text" onChange={handleChange} value={inp}/>
+        <input type="text" placeholder="(555) 555-5555" onChange={handleChange} value={inp} />
         <button onClick={handleClick}>Submit</button>
     </div>
   )
